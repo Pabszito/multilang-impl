e979f4670d87eac4e27c9f54b3df78866255baad
Pabszito <pabszito@gmail.com>
2023-02-10 22:31:23 -0300

    feat(service): add onEnable/onDisable messages

--- src/main/java/com/comugamers/i18n/service/main/MainService.java
+++ src/main/java/com/comugamers/i18n/service/main/MainService.java
@@ -1,6 +1,7 @@
 package com.comugamers.i18n.service.main;
 
 import com.comugamers.i18n.service.Service;
+import org.bukkit.plugin.Plugin;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -14,14 +15,24 @@ public class MainService implements Service {
     @Inject @Named("listener")
     private Service listenerService;
 
+    @Inject
+    private Plugin plugin;
+
     @Override
     public void start() {
         startMany(commandService, listenerService);
+
+        // Send onEnable message
+        plugin.getLogger().info("MultiLangImpl has been enabled!");
+        plugin.getLogger().info("Currently running version " + plugin.getDescription().getVersion() + ".");
     }
 
     @Override
     public void stop() {
         stopMany();
+
+        // Send onDisable message
+        plugin.getLogger().info("MultiLangImpl has been disabled!");
     }
 
     private void startMany(Service... services) {
